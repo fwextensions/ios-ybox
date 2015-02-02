fe2137d54f984488f8aae068bc5d1563e9542de8
John Dunning <jdunning@yahoo-inc.com>
2015-02-01 19:38:19 -0800

    Initial Commit

---
+++ Ybox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ybox
+//
+//  Created by yahoo on 2/1/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
