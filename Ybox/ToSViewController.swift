46b949a7c5622c9b51e77f43639521f11d3cf951
John Dunning <jdunning@yahoo-inc.com>
2015-02-02 23:02:10 -0800

    Initial and final commit.

---
+++ Ybox/ToSViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ToSViewController.swift
+//  Ybox
+//
+//  Created by yahoo on 2/2/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ToSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+	@IBAction func onDone(sender: AnyObject) {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
